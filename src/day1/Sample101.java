4575e9d2a08aebe16373b157db51a256dfb9da16
kosef <kosef@DESKTOP-1HF2H2K>
2024-08-12 03:22:35 +0900

    [Add] サンプルプログラムの作成

--- src/day1/Sample101.java
+++ src/day1/Sample101.java
@@ -1,5 +1,11 @@
 package day1;
 
 public class Sample101 {
+	/*
+	 * Javaを使った最も基本的なプログラム
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello,World."); // 「Hellow,World.」という文字列を表示します。
+	}
 
 }
