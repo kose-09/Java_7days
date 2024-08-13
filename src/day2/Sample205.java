f51420e1beda9be539b02fb4d87327829cf55477
kose-09 <kose09job@gmail.com>
2024-08-14 03:42:57 +0900

    [Add] Sample205

---
+++ src/day2/Sample205.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Sample205 {
+	public static void main(String[] args) {
+		String s1,s2,s3;
+		s1 = "ABC";    //s1に文字列「ABC」を代入
+		s2 = "DEF";    //s2に文字列「DEF」を代入
+		s3 = s1 + s2;  //s3にs1とs2に結合したものを代入
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1 + s2);
+		System.out.println(s3);
+	}
+
+}
