bb6fb2ba9d8666b613cd78ce5bfa144417ab7a32
kose-09 <kose09job@gmail.com>
2024-08-16 02:51:52 +0900

    [Add] Sample401

---
+++ src/day4/Sample401.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Sample401 {
+	
+	public static void main(String[] args) {
+		for(int i = 12; i > 0; i-=3) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+}
