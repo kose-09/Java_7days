1d53aa1e7877f6a171726614257d025f172d68f3
kose-09 <kose09job@gmail.com>
2024-08-16 01:51:04 +0900

    [Add] Prob306

---
+++ src/day3/Prob306.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Prob306 {
+	
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*6)+1;
+		System.out.println("数値：" + num);
+		if(num >= 3) {
+			System.out.println("3以上です。");
+		}
+	}
+
+}
