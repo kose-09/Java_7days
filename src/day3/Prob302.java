5a21b4b512a52f44480d5b2b4390d518840539b9
kose-09 <kose09job@gmail.com>
2024-08-16 01:34:27 +0900

    [Add] Prob302

---
+++ src/day3/Prob302.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Prob302 {
+	
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*10)+1;
+		System.out.println("num = " + num);
+		if(num != 1) {
+			System.out.println("1ではありません。");
+		}
+	}
+
+}
