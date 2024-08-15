7e726cb27334f6f970cb58f1c1f758c99a71c990
kose-09 <kose09job@gmail.com>
2024-08-16 01:38:05 +0900

    [Add] Prob303

---
+++ src/day3/Prob303.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Prob303 {
+	
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*100)+1;
+		System.out.println("num = " + num);
+		if(num < 50) {
+			System.out.println("50未満です。");
+		}
+	}
+
+}
