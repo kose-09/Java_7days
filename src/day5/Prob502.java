3ee88b263be69add627c656b386bc0d29bea04b3
kose-09 <kose09job@gmail.com>
2024-08-19 03:28:28 +0900

    [Add] Prob502

---
+++ src/day5/Prob502.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Prob502 {
+	
+	public static void main(String[] args) {
+		double d[] = {0.2, -5.1, 3.2, 1.8 };
+		for(int i = 0; i < d.length; i++) {
+			System.out.print("d[" +i+ "]=" + d[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
