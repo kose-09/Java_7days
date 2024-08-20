3d50fe69b220f05353e17a2333ac3e9f73744328
kose-09 <kose09job@gmail.com>
2024-08-21 00:31:24 +0900

    [Update] Prob503の乱数の生成をforループ内にまとめる

--- src/day5/Prob503.java
+++ src/day5/Prob503.java
@@ -4,16 +4,11 @@ public class Prob503 {
 	
 	public static void main(String[] args) {
 		int[] data = new int[10];
-		data[0] = (int)(Math.random()*100)+1;
-		data[1] = (int)(Math.random()*100)+1;
-		data[2] = (int)(Math.random()*100)+1;
-		data[3] = (int)(Math.random()*100)+1;
-		data[4] = (int)(Math.random()*100)+1;
-		data[5] = (int)(Math.random()*100)+1;
-		data[6] = (int)(Math.random()*100)+1;
-		data[7] = (int)(Math.random()*100)+1;
-		data[8] = (int)(Math.random()*100)+1;
-		data[9] = (int)(Math.random()*100)+1;
+		
+		for(int i = 0; i < data.length; i++) {
+			data[i] = (int)(Math.random()*100)+1;
+		}
+		
 		for(int i = 0; i < data.length; i++) {
 			System.out.print(data[i] + " ");
 		}
