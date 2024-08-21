1741c7247c96c8d428fef6f60088a765bb8ce970
kose-09 <kose09job@gmail.com>
2024-08-22 01:14:55 +0900

    [Add] Prob310

---
+++ src/day3/Prob310.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Prob310 {
+	
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*100)+1;
+		System.out.println("num = " + num);
+		if(num <= 10 || num >= 90) {
+			System.out.println("10以下か90以上の値です");
+		}else {
+			System.out.println("10より大きく90未満です");
+		}
+	}
+
+}
