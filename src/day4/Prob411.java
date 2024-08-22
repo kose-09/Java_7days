d5a1211db9e7be290e4c5b8b9e36351575da86a8
kose-09 <kose09job@gmail.com>
2024-08-23 01:55:35 +0900

    [Add] Prob411

---
+++ src/day4/Prob411.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class Prob411 {
+	
+	public static void main(String[] args) {
+		int max = 0;
+		for(int i = 0; i < 5; i++) {
+			int num = (int)(Math.random()*100)+1;
+			System.out.println(num);
+			if(num > max) {
+				max = num;
+			}
+		}
+		System.out.println("最大値：" + max);
+	}
+
+}
