13f836454183ef899f23079c5930c9f64dd96682
kose-09 <kose09job@gmail.com>
2024-08-23 01:55:04 +0900

    [Add] Prob410

---
+++ src/day4/Prob410.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Prob410 {
+	
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			int num = (int)(Math.random()*100)+1;
+			System.out.println(num);
+		}
+	}
+
+}
