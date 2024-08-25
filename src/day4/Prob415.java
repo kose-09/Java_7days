1e0cad5e1d9b2137f1eb97f8d44905c8899fc87b
kose-09 <kose09job@gmail.com>
2024-08-25 23:06:49 +0900

    [Add] Prob415

---
+++ src/day4/Prob415.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class Prob415 {
+	
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			System.out.print(i + " ");
+			if(i%10 == 0) {
+				System.out.println();
+			}
+		}
+	}
+
+}
