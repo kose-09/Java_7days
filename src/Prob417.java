8d6086f5103100e03f14e41d6ebae2414836377f
kose-09 <kose09job@gmail.com>
2024-08-26 01:01:53 +0900

    [Add] Prob417

---
+++ src/Prob417.java
@@ -0,0 +1,17 @@
+
+public class Prob417 {
+	
+	public static void main(String[] args) {
+		for(int m = 1; m <= 10; m++) {
+			for(int n = 1; n <= 10; n++) {
+				if(m >= n) {
+					System.out.print("■ ");
+				}else {
+					System.out.print("□ ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
