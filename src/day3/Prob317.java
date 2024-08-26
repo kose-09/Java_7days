ddbfb0d8f523488e946aecccde50dc7271ad63b2
kose-09 <kose09job@gmail.com>
2024-08-27 01:20:24 +0900

    [Add] Prob317

---
+++ src/day3/Prob317.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Prob317 {
+	
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*100)+1;
+		System.out.println("num = " + num);
+		if(num <= 50 && num % 2 == 0) {
+			System.out.println("50以下の偶数です。");
+		}else if(num <= 50) {
+			System.out.println("50以下です。");
+		}else if(num % 2 == 0) {
+			System.out.println("偶数です。");
+		}
+	}
+
+}
