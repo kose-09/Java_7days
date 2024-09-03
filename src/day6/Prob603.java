433967b282cb2dde25f422fec9fe89780015a0cd
kose-09 <kose09job@gmail.com>
2024-09-04 00:28:23 +0900

    [Add] Prob603

---
+++ src/day6/Prob603.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class Prob603 {
+	
+	public static void main(String[] args) {
+		Circle c = new Circle();
+		//円の半径を設定
+		c.r = 4.0;
+		System.out.println("半径" + c.r + "の円の円周の長さは" + c.circumference());
+		System.out.println("半径" + c.r + "の円の面積の大きさは" + c.area());
+	}
+
+}
