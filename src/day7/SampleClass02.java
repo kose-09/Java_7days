f02864ca49199c54f07279650ddc92edceab66e6
kose-09 <kose09job@gmail.com>
2024-09-07 00:19:39 +0900

    [Fix] SampleClass02

--- src/day7/SampleClass02.java
+++ src/day7/SampleClass02.java
@@ -1,5 +1,26 @@
 package day7;
 
 public class SampleClass02 {
+	
+	//コンストラクタ
+	public SampleClass02() {
+		System.out.println("コンストラクタ");
+	}
+	//privateメソッド
+	private void method1() {
+		System.out.println("method1(prinvate)");
+	}
+	//publicメソッド①
+	public void method2() {
+		System.out.println("method(public)");
+	}
+	//publicメソッド②
+	public void method3() {
+		//privateメソッドの呼び出し
+		method1();
+		System.out.println("method3(public) : num=" + this.num);
+	}
+	//privateフィールド
+	private int num = 1;
 
 }
