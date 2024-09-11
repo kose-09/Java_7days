b59342ab3c103b730812e4fe5d9e65f28fcc5bfd
kose-09 <kose09job@gmail.com>
2024-09-11 23:55:53 +0900

    [Add] Data

---
+++ src/day7/Data.java
@@ -0,0 +1,27 @@
+package day7;
+
+public class Data {
+		//メンバ変数number
+		private int number = 0;
+		//メンバ変数comment
+		private String comment = "";
+		//number変数のセッター
+		public void setNumber(int number) {
+			this.number = number;
+		}
+		//comment変数のセッター
+		public void setComment(String comment) {
+			this.comment = comment;
+		}
+		//number変数のゲッター
+		public int getNumber() {
+			return this.number;
+		}
+		//comment変数のゲッター
+		public String getComment() {
+			return this.comment;
+		}
+		
+	}
+
+
--- src/day7/Prob701.java
+++ src/day7/Prob701.java
@@ -1,5 +1,13 @@
 package day7;
 
 public class Prob701 {
+	
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.setNumber(100);
+		d.setComment("Programming Java");
+		System.out.println("number = " + d.getNumber());
+		System.out.println("comment = " + d.getComment());
+	}
 
 }
