a0c401ee28b815660fb5d0a911e1b0b94686b8ec
kose-09 <kose09job@gmail.com>
2024-09-19 01:10:02 +0900

    [Add] TwoStrings

---
+++ src/day7/TwoStrings.java
@@ -0,0 +1,29 @@
+package day7;
+
+public class TwoStrings {
+	//一つ目の文字列
+	private String string1;
+	//二つ目の文字列
+	private String string2;
+	//一つ目の文字列を設定
+	public void setString1(String string1) {
+		this.string1 = string1;
+	}
+	//二つ目の文字列を設定
+	public void setString2(String string2) {
+		this.string2 = string2;
+	}
+	//一つ目の文字列を取得
+	public String getString1() {
+		return string1;
+	}
+	//二つ目の文字列を取得
+	public String getString2() {
+		return string2;
+	}
+	//二つの文字列を結合したものを取得
+	public String getConnectedString() {
+		return string1 + string2;
+	}
+
+}
